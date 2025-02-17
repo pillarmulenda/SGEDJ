0a76511569bc0fc566cabf694135a735b2ecfdcc
Pillar MULENDA <pilardmulenda2@gmail.com>
2025-02-17 20:10:26 +0100

    Premier commit

---
+++ src/main/java/com/SGEDJ/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.SGEDJ.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SGEDJ/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SGEDJ.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
