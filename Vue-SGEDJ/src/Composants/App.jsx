fef255642ab612086d944e6fcfd905c1ff98c25f
Pillar MULENDA <pilardmulenda2@gmail.com>
2025-02-22 14:45:29 +0100

    second commit

---
+++ Vue-SGEDJ/src/Composants/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div><p>salut je suis un homme</p></div>
+  )
+}
+
+export default App
